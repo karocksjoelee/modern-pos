42eb93b09c09dabc097767a3bc58a2d3c2e98b9f
karocksjoelee <karocksjoelee@gmail.com>
2017-06-05 22:58:49 +0800

    [Angular] Feature : Login & Sidebar basic styles
    Color is not yet settled

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,6 +9,16 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
+  openNav() {
+    document.getElementById('mySidenav').style.width = '180px';
+    document.getElementById('main').style.marginLeft = '180px';
+  }
+
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
+
   ngOnInit() {
   }
 
