2249921f3a0ab22b2d59b0c4c8d7f250da0e77e2
Rong Kai Chen <ez32451@gmail.com>
2017-06-18 12:54:35 +0800

    adjust module, add databalse operation

--- routes/building.js
+++ routes/building.js
@@ -10,38 +10,105 @@ const Building = require('../models/buildings');
 // RESTful API 
 // ==========================================================================================
 router.post('/', (req, res) => {
-    // Accept an Object according to schema 
-    // Object will be in req.body
-    res.status(201).send('Created Building'); 
+  // Accept an Object according to schema 
+  // Object will be in req.body
+  let building = new Building({
+    name: req.body.name,
+    category: req.body.category,
+    address: req.body.address,
+    lat: req.body.lat,
+    lng: req.body.lng,
+    description: req.body.description
+  });
+
+  building.save((err, building) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(building);
+    }
+  });
 });
 
 
 router.get('/(:id)?', (req, res) => {
 
-    if(!req.params.id){
-        // if doesn't provide id in url params , return all 
-        res.status(200).send('All Buildings');
-    }else {
-        // Or get item by id 
-        res.status(200).send('Buildings By Id');
-    }
+  if (!req.params.id) {
+    // if doesn't provide id in url params , return all 
+
+    Building.find({}, (err, building) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(building);
+      }
+    });
+  } else {
+    // Or get item by id 
+    Building.find({
+      _id: req.params.id
+    }, (err, building) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(building);
+      }
+    });
+  }
 
 });
 
 
 router.put('/:id', (req, res) => {
-    // Find specific item of id and update it .
-    // Object will be in req.body 
-    res.status(201).send('Updated Building');
+  // Find specific item of id and update it .
+  // Object will be in req.body 
+  Building.findOne({
+    _id: req.params.id
+  }, (err, building) => {
+    building.name = req.body.name || building.name;
+    building.category = req.body.category || building.category;
+    building.address = req.body.address || building.address;
+    building.lat = req.body.lat || building.lat;
+    building.lng = req.body.lng || building.lng;
+    building.description = req.body.description || building.description;
+
+    building.save((err, building) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(building);
+      }
+    });
+  })
+
 });
 
 
 router.delete('/:id', (req, res) => {
-    res.status(200).send('building.name Deleted');
+  Building.findOne({
+    _id: req.params.id
+  }, (err, building) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+
+    building.remove((err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send("Building Deleted!!")
+      }
+    });
+  });
+
 });
 
 
 
 module.exports = router;
-
-
--- routes/item.js
+++ routes/item.js
@@ -12,7 +12,29 @@ const Item = require('../models/item');
 router.post('/', (req, res) => {
   // Accept an Object according to schema 
   // Object will be in req.body
-  res.status(201).send('Created Item');
+  let item = new Item({
+    name: req.body.name,
+    category: req.body.category,
+    barcode: req.body.barcode,
+    price: req.body.price,
+    unit: req.body.unit,
+    image: req.body.image,
+    calorie: req.body.calorie,
+    ingredient: req.body.ingredient,
+    description: req.body.description,
+    active: req.body.active
+  });
+
+  item.save((err, item) => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(item);
+    }
+
+  });
 });
 
 
@@ -20,29 +42,102 @@ router.get('/(:id)?', (req, res) => {
 
   if (!req.params.id) {
     // if doesn't provide id in url params , return all 
-    res.status(200).send('All Items');
+    Item.find({})
+      .populate("accountSubject")
+      .exec((err, items) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(items);
+        }
+      });
+
   } else {
     // Or get item by id 
-    res.status(200).send('Item By Id');
+    Item.find({
+        _id: req.params.id
+      })
+      .populate("accountSubject")
+      .exec((err, item) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(item);
+        }
+      });
   }
 
 });
 
 
 router.get('/activedItem', (req, res) => {
-  res.status(200).send('if item.active === true');
+
+  Item.find({
+      active: true
+    })
+    .populate('accountSubject')
+    .exec((err, items) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(items);
+      }
+    });
+
 });
 
 
 router.put('/:id', (req, res) => {
   // Find specific item of id and update it .
   // Object will be in req.body 
-  res.status(201).send('Updated Item');
+  Item.findOne({
+    _id: req.params.id
+  }, (err, item) => {
+
+    if (err) return res.status(500).send(err);
+
+    item.name = req.body.name || item.name;
+    item.category = req.body.category || item.category;
+    item.barcode = req.body.barcode || item.barcode;
+    item.price = req.body.price || item.price;
+    item.unit = req.body.unit || item.unit;
+    item.image = req.body.image || item.image;
+    item.calorie = req.body.calorie || item.calorie;
+    item.ingredient = req.body.ingredient || item.ingredient;
+    item.description = req.body.description || item.description;
+    item.active = req.body.active || item.active;
+
+    item.save((err, item) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(item);
+        res.status(200).send(item);
+      }
+    });
+
+  });
 });
 
 
 router.delete('/:id', (req, res) => {
-  res.status(200).send('item.name Deleted');
+
+  Item.findOne({
+    _id: req.params.id
+  }, (err, item) => {
+    if (err) return res.status(500).send(err);
+
+    item.remove((err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send('item Deleted')
+      }
+    });
+  });
 });
 
 
