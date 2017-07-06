e6bf082dd3584dadd9ee1afbe181f53e87d09358
karocksjoelee <karocksjoelee@gmail.com>
2017-07-07 00:34:54 +0800

    [Angular] Member List Live Update

--- src/app/member-mgt/member-list/member-list.component.ts
+++ src/app/member-mgt/member-list/member-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MemberMgtService } from '../member-mgt.service';
+import { ModalDirective, ModalOptions } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-member-list',
@@ -10,16 +12,84 @@ import { MemberMgtService } from '../member-mgt.service';
 export class MemberListComponent implements OnInit {
 
   members;
+  options: ModalOptions;
+  selectedMember;
+  inputForm: FormGroup;
+  @ViewChild('addUnExchangedModal') addUnExchangedModal: any;
+   @ViewChild('exChangingModal') exChangingModal: any;
 
   constructor(private _memberMgtService: MemberMgtService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
 
-    this._memberMgtService.getMembers().subscribe((buildings) => {
-      this.members = buildings;
+    this.options = {
+      backdrop: false
+    };
+
+    this._memberMgtService.getMembers().subscribe((members) => {
+      this.members = members;
       console.log(this.members);
+    });
+
+    this.inputForm = new FormGroup({
+      'unExchanged': new FormControl()
     });
 
   }
 
-}
+  onSelectMember(member) {
+    this.selectedMember = member;
+  }
+
+  increaseMemberUnExchanged() {
+
+    const increased = this.selectedMember.unExchanged + this.inputForm.value.unExchanged;
+
+    this._memberMgtService.updateMember(this.selectedMember._id, { unExchanged: increased }).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        console.log('UPDATED');
+        this._memberMgtService.getMembers().subscribe((members) => {
+          this.members = members;
+          this.inputForm = new FormGroup({
+            'unExchanged': new FormControl()
+          });
+          this.addUnExchangedModal.hide();
+        });
+      }
+    );
+
+  } // end of increaseMemberUnExchanged()
+
+  exchanging() {
+
+    const increased = this.selectedMember.exchanged + this.inputForm.value.unExchanged;
+    const decreased = this.selectedMember.unExchanged - this.inputForm.value.unExchanged;
+
+    this._memberMgtService.updateMember(this.selectedMember._id, { unExchanged: decreased , exchanged: increased }).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        console.log('EXCHANGED');
+        this._memberMgtService.getMembers().subscribe((members) => {
+          this.members = members;
+          this.inputForm = new FormGroup({
+            'unExchanged': new FormControl()
+          });
+          this.exChangingModal.hide();
+        });
+      }
+    );
+
+  }
+
+} // end of class
