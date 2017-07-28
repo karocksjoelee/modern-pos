aba375b3926aa61f3b046ffe17d64f09e7db5ff1
karocksjoelee <karocksjoelee@gmail.com>
2017-07-29 02:11:13 +0800

    [Angular] Order Create - Member Point is now correct

--- src/app/pre-order/pre-order-create/pre-order-create.component.ts
+++ src/app/pre-order/pre-order-create/pre-order-create.component.ts
@@ -116,7 +116,7 @@ export class PreOrderCreateComponent implements OnInit {
     this.ngMealSetsModal = true;
   }
 
-   closeMembersModal() {
+  closeMembersModal() {
     this.ngMembersModal = false;
   }
 
@@ -135,21 +135,89 @@ export class PreOrderCreateComponent implements OnInit {
 
   newOrder() {
 
+    if (this.createForm.value.deliverDateTime === '') {
+      const toastErrOption: ToastOptions = {
+          title: '操作提示',
+          msg: `請選擇取餐時間`,
+          timeout: 2000,
+          theme: 'bootstrap'
+      };
+      this._toastyService.error(toastErrOption);
+      return;
+    }
+    // Programatic Writing
+    if (this.createForm.value.deliverDateTime.formatted) {
+      this.createForm.patchValue({deliverDateTime: this.createForm.value.deliverDateTime.formatted});
+    }
     this.createForm.patchValue({ createDate: moment().format()});
+    const humanReadCode = `${this.createForm.value.deliverDateTime.replace('/', '').replace('/', '')}:${Math.floor(Math.random() * 9999) + 1}`;
+    console.log(humanReadCode);
+    this.createForm.patchValue({ orderCode: humanReadCode});
+    // Store Object to id;
+    this.createForm.value.orderedItems.map((ordered) => {
+      return ordered.itemId = ordered.itemId._id;
+    });
+    this.createForm.value.orderedMealSets.map((ordered) => {
+      return ordered.mealSetId = ordered.mealSetId._id;
+    });
+
+    this._preorderService.createSale(this.createForm.value).subscribe(
+      (createdSaleResponse) => {
+        const toastSuccessOption: ToastOptions = {
+          title: '操作提示',
+          msg: `新訂單 : ${this.createForm.value.orderCode}`,
+          timeout: 4000,
+          theme: 'bootstrap'
+        };
+        this._toastyService.success(toastSuccessOption);
+        if (this.selectedMember) {
+          this._memberMgtService.updateMember(this.selectedMember._id, this.selectedMember).subscribe(
+            (memberResponse) => {
+              const toastMemberSuccessOption: ToastOptions = {
+                title: '操作提示',
+                msg: `會員 - ${this.selectedMember.name} 累積紅利已更新`,
+                timeout: 4000,
+                theme: 'bootstrap'
+              };
+              this._toastyService.success(toastMemberSuccessOption);
+            },
+            (memberErr) => {
+              console.log(memberErr);
+            },
+            () => {
+              // Member Process Completed
+              this.router.navigate(['../preorders'], {relativeTo: this.route});
+            }
+          );
+        } else {
+          this.router.navigate(['../preorders'], {relativeTo: this.route});
+        }
+      },
+      (saleErr) => {
+        console.log(saleErr);
+      },
+      () => {
+        // Sale Process Completed
+      }
+    );
 
     console.log(this.createForm.value);
+    console.log(this.selectedMember);
 
   } // end of newOrder()
 
   onSelectedMember(member) {
-
+    if (this.selectedMember) {
+      this.clearMember();
+    }
     this.discountArray = [];
     this.createForm.patchValue({ buyer: member});
     this.createForm.patchValue({ buyerName: member.name });
     this.createForm.patchValue({ note: ''} );
     this.createForm.patchValue({ phone: member.phone });
     this.preUnExchanged = this.createForm.value.buyer.unExchanged;
-    this.preExchanged = this.createForm.value.exChanged;
+    this.preExchanged = this.createForm.value.buyer.exchanged;
+    this.selectedMember = this.createForm.value.buyer;
 
     const discount = Math.floor(this.preUnExchanged / 1000);
 
@@ -157,14 +225,9 @@ export class PreOrderCreateComponent implements OnInit {
       this.discountArray.push(100);
     }
 
-    if (this.discountClick > 0) {
-      this.discountClick = null;
-    }
-
-    // this.calculateTotal(false);
     this.closeMembersModal();
 
-  }
+  } // end of onSelectedMember()
 
 
   onSelectedBuilding(building) {
@@ -191,6 +254,17 @@ export class PreOrderCreateComponent implements OnInit {
 
   clearMember() {
 
+    this.createForm.patchValue({ buyerDiscount: 0});
+    this.createForm.patchValue({ buyer: null });
+    this.createForm.patchValue({ buyerName: '' });
+    this.createForm.patchValue({ phone: ''});
+    this.createForm.patchValue({ note: ''});
+    this.selectedMember = null;
+
+    if (this.discountClick > 0) {
+      this.discountClick = 0;
+    }
+
     const toastWarnOption: ToastOptions = {
         title: '操作提示',
         msg: '會員資料清除',
@@ -200,15 +274,7 @@ export class PreOrderCreateComponent implements OnInit {
 
     this._toastyService.warning(toastWarnOption);
 
-    this.createForm.patchValue({ buyer: null });
-    this.createForm.patchValue({ buyerName: '' });
-    this.createForm.patchValue({ phone: ''});
-    this.createForm.patchValue({ note: ''});
-    this.selectedMember = null;
-
-    if (this.discountClick > 0) {
-      this.discountClick = null;
-    }
+    this.caculateTotal();
 
   } // end of clearMember()
 
@@ -359,12 +425,25 @@ export class PreOrderCreateComponent implements OnInit {
       this.discountClick++;
       this.discountArray.pop();
       const progrmaticNote = this.createForm.value.note.concat('\n[已使用一張折價券]');
+      this.createForm.patchValue({buyerDiscount: this.discountClick});
       this.createForm.patchValue({note: progrmaticNote});
+      this.caculateTotal();
+
+      const toastSuccessOption: ToastOptions = {
+        title: '操作提示',
+        msg: '已使用一張折價券',
+        timeout: 4000,
+        theme: 'bootstrap'
+      };
+      this._toastyService.success(toastSuccessOption);
     }
 
   } // end of onExchanging()
 
   caculateTotal() {
+
+    console.log('BuyerDiscount: ', this.createForm.value.buyerDiscount);
+    this.createForm.patchValue({businessMemberPoint: null});
 
     const itemsTotal = this.createForm.value.orderedItems.reduce((total, ordered) => {
       return total + ordered.itemId.price * ordered.quantity;
@@ -376,6 +455,40 @@ export class PreOrderCreateComponent implements OnInit {
 
     this.beforeDiscount = itemsTotal + mealSetsTotal;
     this.createForm.patchValue({total: this.beforeDiscount});
+
+    if (this.createForm.value.buyer) {
+
+      if (this.createForm.value.buyerDiscount > 0) {
+        const afterDiscount = this.beforeDiscount - 100 * this.createForm.value.buyerDiscount;
+        this.createForm.patchValue({total: afterDiscount});
+        if (this.createForm.value.buyer.type === 'business' && this.createForm.value.total >= 1000) {
+          // (1) Give Business Bonus Point :
+          const bonusWeight = Math.floor(this.createForm.value.total / 1000);
+          this.selectedMember.unExchanged = this.preUnExchanged + 100 * bonusWeight;
+          this.createForm.patchValue({businessMemberPoint: 100 * bonusWeight});
+          // (2) Increase Member UnExchanged (After Discount)
+          this.selectedMember.unExchanged = this.preUnExchanged + 100 * bonusWeight + this.createForm.value.total;
+          // (3) Increase Member ExChanged (After Discount)
+          this.selectedMember.exchanged = this.preExchanged + 1000 * this.createForm.value.buyerDiscount;
+        } else {
+          this.selectedMember.unExchanged = this.preUnExchanged + this.createForm.value.total;
+          this.selectedMember.exchanged = this.preExchanged + 1000 * this.createForm.value.buyerDiscount;
+        }
+      } else {
+        if (this.createForm.value.buyer.type === 'business' && this.createForm.value.total >= 1000) {
+          // Give Business Bonus Point :
+          const bonusWeight = Math.floor(this.createForm.value.total / 1000);
+          this.selectedMember.unExchanged = this.preUnExchanged + 100 * bonusWeight;
+          this.createForm.patchValue({businessMemberPoint: 100 * bonusWeight});
+          // Increase Member UnExChanged (Non Discount)
+          this.selectedMember.unExchanged = this.preUnExchanged + 100 * bonusWeight + this.createForm.value.total;
+        } else {
+          this.selectedMember.unExchanged = this.preUnExchanged + this.createForm.value.total;
+        }
+      } // end of check if else discounted
+
+    } // end of check if isMember;
+
   } // end of caculateTotal()
 
 }
